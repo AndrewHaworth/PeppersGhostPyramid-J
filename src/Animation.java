ab8c70b61cf316a60bc7d22bce40c248bc980b25
AndrewHaworth <the_andy_haworth@hotmail.com>
2018-04-11 22:46:57 +0100

    Added rudimentary face detection, bugs in detectmultiscale

--- src/Animation.java
+++ src/Animation.java
@@ -1,15 +1,23 @@
 import javafx.scene.image.Image;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
 
 
 public class Animation extends VideoOutput {
-    private Mat frame = new Mat();
+    private CascadeClassifier faceCascade = new CascadeClassifier();
+    private int faceSize = 0;
+    private Mat frame = new Mat(), gray = new Mat();
+    private MatOfRect faces = new MatOfRect();
+
+    public Animation(){
+        faceCascade.load("faceDetector.xml");
+    }
 
     public Image animate(VideoCapture capture) {
-        return createImage(duplicate(grabFrame(capture)));
+        return createImage(detect(grabFrame(capture)));
     }
 
     public Mat grabFrame(VideoCapture capture) {
@@ -23,5 +31,20 @@ public class Animation extends VideoOutput {
             System.out.println("Oh no! The frame is empty. Sending blank.");
             return new Mat(1920, 1080, CvType.CV_8U, new Scalar(128, 128, 128));
         }
+    }
+
+    private Mat detect(Mat frame) {
+        int height = frame.rows();
+        Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.equalizeHist(gray, gray);
+        if (faceSize == 0)
+            if (Math.round(height * 0.2) > 0)
+                faceSize = (int) Math.round(height * 0.2);
+        faceCascade.detectMultiScale(gray, faces, 1.1, 2, Objdetect.CASCADE_SCALE_IMAGE, new Size(faceSize, faceSize), new Size());
+        Rect[] faceArray = faces.toArray();
+        for (Rect face : faceArray) {
+            Imgproc.rectangle(frame, face.tl(), face.br(), new Scalar(255, 0, 0), 1);
+        }
+        return frame;
     }
 }
